98d04752b381b1bfbb2f36a3c92036e9bce9db2f
ChronoAero <75560157+ChronoAero@users.noreply.github.com>
2022-02-22 07:44:59 +0800

    added security fixes

--- client/src/app/index.ts
+++ client/src/app/index.ts
@@ -10,13 +10,15 @@ export const csrfProtection = csurf({ cookie: true })
 const app = express();
 app.disable('x-powered-by');
 app.use(cookieParser())
+app.use(require('express-limit').limit({
+    max:    5,       
+    period: 60 * 1000 
+}))
+app.use(csrfProtection)
 
 app.use(express.static(path.join(__dirname, '../../../client/dist')))
 
-app.get('*', [require('express-limit').limit({
-    max:    5,        // 5 requests
-    period: 60 * 1000 // per minute (60 seconds)
-}), csrfProtection], (req:Request, res:Response) => { 
+app.get('*', (req:Request, res:Response) => { 
     res.sendFile(path.join(__dirname, '../../../client/dist/view/index.html'))
 });
 
